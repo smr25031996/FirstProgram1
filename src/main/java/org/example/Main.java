ff82e8681bd99808fb891acbd424664a8f4e3de2
Shubham <rajputshubham615@gmail.com>
2022-09-12 20:57:33 +0530

    Wrote the basic program of hello World using static method

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Hello World !");
+    }
+}
